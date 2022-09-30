7e41070d328613256e3e6adb4e6d0a15b0feb208
Anish Chikhaliya <anishchikhaliya02@gmail.com>
2022-09-29 20:18:53 -1200

    Add Animation

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,13 +1,21 @@
 import LogoTitle from '../../assets/images/logo-s.png';
-
+import { Link } from 'react-router-dom';  
+import './index.scss';
 
 const Home = () => {
 
-  return (
+  return (  
+    
     <div className="container home-page">
        <div className="text-zone">
-            <h1>Hi, <br/> I'm </h1>
-            <img src={LogoTitle} alt="developer" />
+            <h1>Hi, <br/> I'm 
+            <img src={LogoTitle} alt="developer" /> 
+            nish
+            <br/>
+            Web developer
+            </h1>
+            <h2>Front-end developer</h2>
+            <Link to="/contact" className="flat-button">CONTACT ME</Link>
 
        </div> 
     </div>
